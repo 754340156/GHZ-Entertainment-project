89fe3e899f16c6b8dcef6de490c31795e29b6607
lanou3g <754340156@qq.com>
2016-07-08 10:26:04 +0800

    搭建直播页面

--- GHZxiangmuB/GHZxiangmuB/Classes/Home/Live/New/Controller/GHZLiveNewViewController.m
+++ GHZxiangmuB/GHZxiangmuB/Classes/Home/Live/New/Controller/GHZLiveNewViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "GHZLiveNewViewController.h"
-
-@interface GHZLiveNewViewController ()<UITableViewDelegate,UITableViewDataSource>
-
+#import <SDCycleScrollView/SDCycleScrollView.h>
+@interface GHZLiveNewViewController ()<UITableViewDelegate,UITableViewDataSource,SDCycleScrollViewDelegate>
+/**  头部轮播视图 */
+@property (nonatomic,strong) SDCycleScrollView *headerCycleView;
 @end
 
 @implementation GHZLiveNewViewController
@@ -25,18 +26,26 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     [self.view addSubview:self.tableView];
+    
 }
 - (void)setHeaderView
 {
     
 }
+#pragma mark - UITableViewDelegate
 - (NSInteger )tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return 5;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return nil;
+    static NSString *cellIdentifier = @"cell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld,%ld",(long)indexPath.section,(long)indexPath.row];
+    return cell;
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -46,4 +55,35 @@
 {
     return 100;
 }
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+
+    self.headerCycleView = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 0, GHZScreenWidth, cycleSVWidth) delegate:self placeholderImage:[UIImage imageNamed:cycleSVPlaceHolder]];
+    self.headerCycleView.imageURLStringsGroup = @[
+                                                  @"https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a4b3d7085dee3d6d2293d48b252b5910/0e2442a7d933c89524cd5cd4d51373f0830200ea.jpg",
+                                                  @"https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a41eb338dd33c895a62bcb3bb72e47c2/5fdf8db1cb134954a2192ccb524e9258d1094a1e.jpg",
+                                                  @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg"
+                                                  ];
+    return self.headerCycleView;
+}
+#pragma mark  - SDCycleScrollViewDelegate
+/** 点击图片回调 */
+- (void)cycleScrollView:(SDCycleScrollView *)cycleScrollView didSelectItemAtIndex:(NSInteger)index
+{
+    NSLog(@"%ld",(long)index);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 @end
