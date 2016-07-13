5a3ac8f67de88d992a5e31e378e0de9c65ea5219
臭婊子 <lanou3g@lanou3gdeMac-mini-2.local>
2016-07-13 22:16:35 +0800

    声音刚开始

---
+++ GHZxiangmuB/GHZxiangmuB/Classes/Category/NSString+GHZTime.h
@@ -0,0 +1,13 @@
+//
+//  NSString+GHZTime.h
+//  GHZxiangmuB
+//
+//  Created by lanou3g on 16/7/13.
+//  Copyright © 2016年 lanou3g-22赵哲. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (GHZTime)
+
+@end
---
+++ GHZxiangmuB/GHZxiangmuB/Classes/Category/NSString+GHZTime.m
@@ -0,0 +1,13 @@
+//
+//  NSString+GHZTime.m
+//  GHZxiangmuB
+//
+//  Created by lanou3g on 16/7/13.
+//  Copyright © 2016年 lanou3g-22赵哲. All rights reserved.
+//
+
+#import "NSString+GHZTime.h"
+
+@implementation NSString (GHZTime)
+
+@end
