34730f754404927084a4bc2b190819c62d0881bb
臭婊子 <lanou3g@lanou3gdeMac-mini-2.local>
2016-07-16 15:04:43 +0800

    提交

--- GHZxiangmuB/GHZxiangmuB/Classes/Home/Profile/Profile/Controller/GHZCollctionListViewController.m
+++ GHZxiangmuB/GHZxiangmuB/Classes/Home/Profile/Profile/Controller/GHZCollctionListViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "GHZCollctionListViewController.h"
-
+#import "GHZCollectionView.h"
+#import "GHZNewCollectionView.h"
 @interface GHZCollctionListViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (nonatomic,strong)UITableView *tableView;
 @property (nonatomic,strong)NSArray *arr;
+@property (nonatomic,strong)UISegmentedControl *segmentedControl;
 @end
 
 @implementation GHZCollctionListViewController
@@ -22,9 +24,27 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     [self.view addSubview:self.tableView];
-    self.arr = @[@"精华",@"新帖"];
+    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"精华",@"最新"]];
+    self.segmentedControl.frame = CGRectMake(100, 60, 150, 50);
+    self.segmentedControl.selectedSegmentIndex = 0;
+    [self.segmentedControl addTarget:self action:@selector(segmentAction:) forControlEvents:(UIControlEventValueChanged)];
+    [self.view addSubview:self.segmentedControl];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
 }
+
+-(void)segmentAction:(id)sender{
+    if ([sender isKindOfClass:[UISegmentedControl class]]) {
+        UISegmentedControl *segment = sender;
+        if (segment.selectedSegmentIndex==0) {
+            
+        }else if (segment.selectedSegmentIndex==1){
+ 
+        }
+  
+    }
+ 
+}
+
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return self.arr.count;
@@ -35,6 +55,18 @@
     return cell;
 }
 
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (indexPath.row==0) {
+        GHZCollectionView *VC = [[GHZCollectionView alloc] init];
+        [self.navigationController pushViewController:VC animated:YES];
+    }else
+        if (indexPath.row==1) {
+            GHZNewCollectionView *VC = [[GHZNewCollectionView alloc] init];
+            [self.navigationController pushViewController:VC animated:YES];
+        }
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
--- GHZxiangmuB/GHZxiangmuB/Classes/Home/Profile/Profile/Controller/GHZNewCollectionView.m
+++ GHZxiangmuB/GHZxiangmuB/Classes/Home/Profile/Profile/Controller/GHZNewCollectionView.m
@@ -1,27 +1,28 @@
 //
-//  GHZCollectionView.m
+//  GHZNewCollectionView.m
 //  GHZxiangmuB
 //
 //  Created by lanou3g on 16/7/16.
 //  Copyright © 2016年 lanou3g-22赵哲. All rights reserved.
 //
 
-#import "GHZCollectionView.h"
+#import "GHZNewCollectionView.h"
 #import "GHZNewWordCell.h"
 #import "AFNetWorking.h"
 #import "GHZTopicModel.h"
 #import "MJExtension.h"
-@interface GHZCollectionView ()<UITableViewDataSource,UITableViewDelegate>
+@interface GHZNewCollectionView ()<UITableViewDataSource,UITableViewDelegate>
 @property (nonatomic,strong)UITableView *tableView;
 @property (nonatomic,strong)NSMutableArray *clloctionArr;
+
 @end
 
-@implementation GHZCollectionView
+@implementation GHZNewCollectionView
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
-
+    // Do any additional setup after loading the view.
+    
     self.tableView = [[UITableView alloc] initWithFrame:self.view.frame style:(UITableViewStylePlain)];
     self.tableView.backgroundColor = GHZRGBColor(223, 223, 223);
     self.tableView.dataSource = self;
@@ -30,11 +31,10 @@
     [self lodingdata];
     [self.tableView registerNib:[UINib nibWithNibName:@"GHZNewWordCell" bundle:nil] forCellReuseIdentifier:@"GHZNewWordCell"];
 }
-
 -(void)lodingdata{
     //参数
     NSMutableDictionary *dic = [NSMutableDictionary dictionary];
-    dic[@"a"] = @"list";
+    dic[@"a"] = @"newlist";
     dic[@"c"] = @"data";
     dic[@"type"] = @"1";
     //self.params = dic;
@@ -50,25 +50,27 @@
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.clloctionArr.count;
+    ;
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-   return self.clloctionArr.count;
-;
-}
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     GHZTopicModel *model = self.clloctionArr[indexPath.row];
     return model.cellHeight;
 }
+
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     GHZNewWordCell *cell = [tableView dequeueReusableCellWithIdentifier:@"GHZNewWordCell"];
     cell.model = self.clloctionArr[indexPath.row];
     return cell;
-
     
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
